98f7c1080765e00954d0c38a98ab1bb3e207c059
Martin Wimpress <code@flexion.org>
2015-11-22 23:20:04 +0000

    Determine what to encode based in skip options.

--- podcoder.py
+++ podcoder.py
@@ -6,20 +6,23 @@
 
 from podpublish import configuration
 from podpublish import encoder
-from podpublish import uploader
 
 def main():
     config = configuration.Configuration('podcoder.ini')
-    encoder.audio_encode(config, 'mp3')
-    encoder.mp3_tag(config)
-    encoder.mp3_coverart(config)
-    encoder.audio_encode(config, 'ogg')
-    encoder.ogg_tag(config)
-    encoder.ogg_coverart(config)
-    encoder.png_header(config)
-    encoder.png_poster(config)
-    encoder.mkv_encode(config)
-    uploader.youtube_upload(config)
+    if not config.mp3['skip']:
+        encoder.audio_encode(config, 'mp3')
+        encoder.mp3_tag(config)
+        encoder.mp3_coverart(config)
+
+    if not config.ogg['skip']:
+        encoder.audio_encode(config, 'ogg')
+        encoder.ogg_tag(config)
+        encoder.ogg_coverart(config)
+
+    if not config.youtube['skip']:
+        encoder.png_header(config)
+        encoder.png_poster(config)
+        encoder.mkv_encode(config)
 
 if __name__ == '__main__':
     main()

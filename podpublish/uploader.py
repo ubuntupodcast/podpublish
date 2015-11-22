21a2114e64e32550c3406c84bebc37c4c6df00bc
Martin Wimpress <code@flexion.org>
2015-11-22 22:52:56 +0000

    Add sftp upload.

--- podpublish/uploader.py
+++ podpublish/uploader.py
@@ -5,14 +5,67 @@
 # See the file "LICENSE" for the full license governing this code.
 
 import optparse
+import pysftp
+import sys
 from youtube_upload import main as yt
+
+def _sftp_put_file(config, cinfo, file_in):
+    with pysftp.Connection(**cinfo) as sftp:
+        print("Connected to: " + config.sftp['host'])
+        print("Making directory: " + config.sftp['remote_directory'])
+        sftp.makedirs(config.sftp['remote_directory'])
+        with sftp.cd(config.sftp['remote_directory']):
+            print("Changed to: " + config.sftp['remote_directory'])
+            print("Putting: " + file_in)
+            sftp.put(file_in)
+            print("Setting permissions: 644")
+            sftp.chmod(file_in, 644)
+
+def sftp_upload(config, file_in):
+    print("Uploading " + file_in + ' via sftp')
+
+    if not config.sftp['remote_directory'].endswith('/'):
+        config.sftp['remote_directory'] += '/'
+        print('Added trailing / to:' + config.sftp['remote_directory'])
+
+    if (config.sftp['username'] and config.sftp['password']) and not config.sftp['private_key']:
+        print("Attempting to authenticate with username and password.")
+        cinfo = {'host': config.sftp['host'],
+                 'username': config.sftp['username'],
+                 'password': config.sftp['password'],
+                 'port': config.sftp['port']}
+
+    elif (config.sftp['username'] and config.sftp['private_key']) and not config.sftp['private_key_pass']:
+        print("Attempting to authenticate with username and private_key.")
+        cinfo = {'host': config.sftp['host'],
+                 'username': config.sftp['username'],
+                 'private_key': config.sftp['private_key'],
+                 'port': config.sftp['port']}
+
+    elif config.sftp['username'] and config.sftp['private_key'] and config.sftp['private_key_pass']:
+        print("Attempting to authenticate with username and private_key that has a passphrase.")
+        cinfo = {'host': config.sftp['host'],
+                 'username': config.sftp['username'],
+                 'private_key': config.sftp['private_key'],
+                 'private_key_pass': config.sftp['private_key_pass'],
+                 'port': config.sftp['port']}
+
+    _sftp_put_file(config, cinfo, file_in)
+
+    # Check the file uploaded correctly.
+    file_check = False
+    with pysftp.Connection(**cinfo) as sftp:
+        print("Connected to: " + config.sftp['host'])
+        file_check = sftp.isfile(config.sftp['remote_directory'] + file_in)
+
+    if not file_check:
+        print('ERROR! Upload failed. Abort.')
+        sys.exit(1)
+    else:
+        print('Upload completed.')
 
 def youtube_upload(config):
     print("Uploading " + config.mkv_file + ' to YouTube')
-    # The reference YouTube API provided by doesn't support playlists,
-    # so wrap youtube-upload instead.
-    #  * https://github.com/tokland/youtube-upload
-    #  * https://www.youtube.com/watch?v=IX8xlnk54Mg
 
     parser = optparse.OptionParser()
     # Video metadata

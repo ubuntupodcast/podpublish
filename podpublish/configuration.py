c256793d393534565771ed88a865666e94a1e22c
Martin Wimpress <code@flexion.org>
2015-11-23 13:34:20 +0000

    Added automatic path expansion. Added file existance checks.

--- podpublish/configuration.py
+++ podpublish/configuration.py
@@ -5,7 +5,15 @@
 # See the file "LICENSE" for the full license governing this code.
 
 import configobj
+import os
+import random
+import sys
 import validate
+
+def check_exists(file_in):
+    if not os.path.isfile(file_in):
+        print('ERROR! ' + file_in + ' was not found. Abort.')
+        sys.exit(1)
 
 class Configuration(object):
 
@@ -13,7 +21,49 @@ class Configuration(object):
         # FIXME: Add error checking
         self.config = configobj.ConfigObj(ini_file)
 
+        # expand any relative file paths.
+        if self.config['artwork']['backdrop'].startswith('~'):
+            self.config['artwork']['backdrop'] = os.path.expanduser(self.config['artwork']['backdrop'])
+
+        if self.config['artwork']['coverart'].startswith('~'):
+            self.config['artwork']['coverart'] = os.path.expanduser(self.config['artwork']['coverart'])
+
+        if self.config['artwork']['font'].startswith('~'):
+            self.config['artwork']['font'] = os.path.expanduser(self.config['artwork']['font'])
+
+        if self.config['episode']['audio_in'].startswith('~'):
+            self.config['episode']['audio_in'] = os.path.expanduser(self.config['episode']['audio_in'])
+
+        if self.config['youtube']['client_secrets'].startswith('~'):
+            self.config['youtube']['client_secrets'] = os.path.expanduser(self.config['youtube']['client_secrets'])
+
+        if self.config['youtube']['credentials_file'].startswith('~'):
+            self.config['youtube']['credentials_file'] = os.path.expanduser(self.config['youtube']['credentials_file'])
+
+        if self.config['sftp']['private_key'].startswith('~'):
+            self.config['sftp']['private_key'] = os.path.expanduser(self.config['sftp']['private_key'])
+
+        # If backdrop is a directory, grab a random file from it.
+        if os.path.isdir(self.config['artwork']['backdrop']):
+            self.config['artwork']['backdrop'] = random.choice(os.listdir(self.config['artwork']['backdrop']))
+
+        # Check that required files exist.
+        check_exists(self.config['artwork']['backdrop'])
+        check_exists(self.config['artwork']['coverart'])
+        check_exists(self.config['artwork']['font'])
+
+        #audio_in can be a directory for season_to_youtube
+        if os.path.isdir(self.config['episode']['audio_in']):
+            pass
+        else:
+            check_exists(self.config['episode']['audio_in'])
+        check_exists(self.config['youtube']['client_secrets'])
+        check_exists(self.config['youtube']['credentials_file'])
+        if self.config['sftp']['private_key']:
+            check_exists(self.config['sftp']['private_key'])
+
         # artwork
+        self.backdrop = self.config['artwork']['backdrop']
         self.coverart = self.config['artwork']['coverart']
         if self.coverart.endswith('.png'):
             self.coverart_mime = 'image/png'
@@ -22,7 +72,6 @@ class Configuration(object):
         elif self.coverart.endswith('.jpg') or self.coverart.endswith('.jpeg'):
             self.coverart_mime = 'image/jpeg'
 
-        self.backdrop = self.config['artwork']['backdrop']
         self.font = self.config['artwork']['font']
         self.font_size = int(self.config['artwork']['font_size'])
         self.font_color = self.config['artwork']['font_color']

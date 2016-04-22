47d2f0bc7f1c1bae0188ab03427f993b4aedf558
Martin Wimpress <code@flexion.org>
2016-04-22 12:17:33 +0100

    Make python packaging more conventional. Bundle youtube_upload.

--- setup.py
+++ setup.py
@@ -6,8 +6,34 @@
 
 import podpublish as pkg
 from setuptools import setup, find_packages
-from requirements import RequirementsParser
-requirements = RequirementsParser()
+
+
+def get_requirements(fname):
+    try:
+        with open(fname, 'r') as fh:
+            requirements = [l.strip() for l in fh]
+    except:
+        requirements = []
+
+    return requirements
+
+
+def get_extras(fname):
+    extras = {}
+
+    try:
+        with open(fname, 'r') as fh:
+            extras['extras'] = [l.strip() for l in fh][1:]
+    except:
+        pass
+
+    return extras
+
+
+install_requires = get_requirements('requirements.txt')
+setup_requires = get_requirements('requirements-setup.txt')
+tests_require = get_requirements('requirements-tests.txt')
+extras_require = get_extras('requirements-extras.txt')
 
 setup(
     name=pkg.__packagename__,
@@ -25,14 +51,22 @@ setup(
     platforms=pkg.__platforms__,
     license=pkg.__license__,
     keywords=pkg.__keywords__,
-    #packages=find_packages(exclude=['lextab.*', 'parsetab.*']),
+    packages=find_packages(exclude=('tests',)),
     include_package_data=True,
     zip_safe=False,
-    install_requires=requirements.install_requires,
-    setup_requires=requirements.setup_requires,
-    tests_require=requirements.tests_require,
-    extras_require=requirements.extras_require,
-    dependency_links=requirements.dependency_links,
+    install_requires=install_requires,
+    extras_require=extras_require,
+    setup_requires=setup_requires,
+    tests_require=tests_require,
+    entry_points={
+        'console_scripts': [
+            'encode-podcast = podpublish.encode_podcast:main',
+            'publish-podcast = podpublish.publish_podcast:main',
+            'season-to-youtube = podpublish.season_to_youtube:main',
+            'youtube-upload = youtube_upload.main:run',
+        ],
+        'gui_scripts' : [],
+    },
     test_suite='nose.collector',
 )
 

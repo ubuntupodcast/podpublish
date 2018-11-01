0cbd84b8ceebe8cc162a1a33c16aa79633e816a9
Martin Wimpress <code@flexion.org>
2018-11-01 02:03:56 +0000

    podpublish: Bump to v0.0.5

--- podpublish/__init__.py
+++ podpublish/__init__.py
@@ -5,7 +5,7 @@
 # See the file "LICENSE" for the full license governing this code.
 
 __packagename__ = 'PodPublish'
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 __author__ = 'Ubuntu Podcast'
 __author_email__ = 'show@ubuntupodcast.org'
 __maintainer__ = 'Ubuntu Podcast'
@@ -14,12 +14,12 @@ __url__= 'http://www.ubuntupodcast.org/'
 __description__ ='A tool for encoding and publishing podcast content and assets.'
 __download_url__= 'https://bitbucket.org/flexiondotorg/podpublish'
 __classifiers__ = [
-    'Development Status :: 4 - Alpha',
     'Environment :: Console',
     'Intended Audience :: Developers',
     'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
     'Operating System :: OS Independent',
     ]
 __platforms__= ['OS Independent']
